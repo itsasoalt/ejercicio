24b5c2b1bf05b44ceed68f61f163f86804659892
itsaso <itsaso.altuna@gmail.com>
2016-05-12 14:36:26 +0200

    correcciones

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -17,30 +17,29 @@ $(document).ready(function(){
 		$('#mobile-close').hide();
 	});
 
-	if ($(window).width()>990) {
-		$('nav>ul>li').hover(function() {
+	$('nav>ul>li').hover(function() {
+		if ($(window).width()>990) {
 			$(this).find('.sub-menu').fadeToggle();
-		});
-	} else {
-		$('nav>ul>li').click(function() {
-			$('nav>ul>li').find($('.sub-menu')).hide();
+		}
+	});
+	
+	$('nav>ul>li').click(function() {
+		if ($(window).width()<=990) {
+			$('nav>ul>li').not(this).find($('.sub-menu:visible')).hide();
 			$(this).find($('.sub-menu')).toggle();
-		});
-	};
+		}	
+	});
 
 	$(window).resize(function() {
-		document.location.reload();
 
-		// if ($(window).width()>990) {
-		// 	$('nav>ul>li').bind('hover');
-		// 	$('#mobile-button').hide();
-		// 	$('#mobile-close').hide();
+		if ($(window).width()>990) {
+			$('#mobile-button').hide();
+			$('#mobile-close').hide();
 
-		// } else {
-		// 	$('nav>ul>li').unbind('hover');
-		// 	$('#menu-container').hide();
-		// 	$('#mobile-button').show();
-		// }
+		} else {
+			$('#menu-container').hide();
+			$('#mobile-button').show();
+		}
 	});
 
 });
